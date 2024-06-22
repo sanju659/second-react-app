df9752717a2857eb09a7f326118fb7291e6954d5
Sanju Mondal <sanjumondal130105@gmail.com>
2024-06-23 00:54:39 +0530

    Jobs listing features using json

---
+++ src/components/JobListings.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import jobs from "./../jobs.json";
+import Joblisting from "./Joblisting";
+
+const JobListings = () => {
+  // console.log(jobs);
+  return (
+    <>
+      <section className="bg-blue-50 px-4 py-10">
+        <div className="container-xl lg:container m-auto">
+          <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+            Browse Jobs
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <Joblisting key={job.id} job={job}/>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default JobListings;
