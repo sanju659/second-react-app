87ae9e789b0574ad58b35d5bf1309f18f42259df
Sanju Mondal <sanjumondal130105@gmail.com>
2024-06-28 20:54:54 +0530

    Conditional rendering feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero"
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import NotFoundPage from "./pages/NotFoundPage";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
+
+const router = createBrowserRouter(
+  // createRoutesFromElements(<Route index element={<HomePage/> } />)
+  // createRoutesFromElements(<Route path="/about" element={<h1>My App</h1> } />)
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage/>} />
+    </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <>
-    <Navbar/>
-    <Hero title="Become a React Dev" subtitle="Find the React job that fits your skills and needs"/>
-    <HomeCards/>
-    <JobListings/>
-    <ViewAllJobs/>      
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/components/Joblisting.jsx
+++ src/components/Joblisting.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useState } from 'react';
+import { FaMapMarker } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Joblisting = ({ job }) => {
     const [showFullDescription, setshowFullDescription] = useState(false);
@@ -27,15 +29,16 @@ const Joblisting = ({ job }) => {
 
           <div className="flex flex-col lg:flex-row justify-between mb-4">
             <div className="text-orange-700 mb-3">
-              <i className="fa-solid fa-location-dot text-lg"></i>
+              {/* <i className="fa-solid fa-location-dot text-lg"></i> */}
+              <FaMapMarker className="inline text-lg mb-1 mr-1"/>
               {job.location}
             </div>
-            <a
-              href={`/jobs/${job.id}`}
+            <Link
+              to={`/jobs/${job.id}`}
               className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
             >
               Read More
-            </a>
+            </Link>
           </div>
         </div>
       </div>
